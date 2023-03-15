3bbc60874d05e114223d428b325fcb4811be8a59
Mohr <contact.cmohr@gmail.com>
2023-03-15 10:50:00 +0100

    add plotting and exporting functionlity

--- R/annotations.R
+++ R/annotations.R
@@ -1,12 +1,12 @@
-#' Read GEL annotation Excel spreadsheet into a list of data frames, per sheet
+#' Read Excel spreadsheet into a list of data frames, per sheet
 #'
 #' @param annotation_data_path Path to annotation .xlsx
+#' @param sheet_names List of names of the sheets in the Excel spreadsheet
 #'
 #' @return list of data.frame
 #' @export
-read_annotation_data <- function(annotation_data_path){
-  sheets <- c("Hotspots", "OPA", "Census", "Colon", "Histiocytosis", "Glioblastoma", "GIST")
-  annotations <- map(sheets, ~ readxl::read_excel(annotation_data_path, sheet = .x) %>% janitor::clean_names())
-  names(annotations) <- sheets
+read_annotation_data <- function(annotation_data_path, sheet_names){
+  annotations <- map(sheet_names, ~ readxl::read_excel(annotation_data_path, sheet = .x) %>% janitor::clean_names())
+  names(annotations) <- sheet_names
   return(annotations)
 }
