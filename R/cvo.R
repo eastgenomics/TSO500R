7c7d38b2c5064f43bc3e9b0abfff3b899fc99ff5
Christopher Mohr <christopher.mohr@boehringer-ingelheim.com>
2023-10-06 10:18:05 +0200

    transform NA strings to NA to avoid warnings

--- R/cvo.R
+++ R/cvo.R
@@ -221,7 +221,8 @@ parse_cvo_record <- function(record_string){
     stringr::str_split("\n") %>%
     unlist() %>%
     stringr::str_remove("\\t$") %>%
-    stringr::str_split("\\t")
+    stringr::str_split("\\t") %>%
+    replace(., .=="NA", NA) # replace all string NAs with NA to avoid warnings from as.numeric
 
   if(stringr::str_detect(record_string, "TMB|MSI")){
     record <- purrr::map(intermediate, ~ as.numeric(.x[2]))
